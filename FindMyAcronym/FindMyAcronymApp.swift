c3fa27a3357f1e696ae0c54ac362d684a884e2b7
pedro-rothen <p.rothen@outlook.cl>
2024-07-15 20:52:14 -0400

    Init

---
+++ FindMyAcronym/FindMyAcronymApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyAcronymApp.swift
+//  FindMyAcronym
+//
+//  Created by Pedro on 15-07-24.
+//
+
+import SwiftUI
+
+@main
+struct FindMyAcronymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
